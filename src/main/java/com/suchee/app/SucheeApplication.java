4148af6f4e74cffbdf903657a9a28ded01fab18c
Jatin Pandey <jatin26pandey@gmail.com>
2025-05-21 01:07:27 +0530

    init git

---
+++ src/main/java/com/suchee/app/SucheeApplication.java
@@ -0,0 +1,13 @@
+package com.suchee.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SucheeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SucheeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/suchee/app/SucheeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suchee.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SucheeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
